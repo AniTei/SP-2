f302eb3c3498f245fa340ced2cf5f733da23844d
AniTei <115176414+AniTei@users.noreply.github.com>
2024-03-22 16:30:36 +0100

    adds files for displaying listings on home

---
+++ js/endpoints.mjs
@@ -0,0 +1,20 @@
+export const baseURL = "https://v2.api.noroff.dev";
+
+export const allListings = "/auction/listings/"
+
+export const bids = "?_bids=true";
+
+
+// id has to be built with the querystinrg 
+
+"https://v2.api.noroff.dev/auction/listings/<id>"
+
+// feks
+// https://v2.api.noroff.dev/auction/listings/e9b647ce-96ee-4fb8-96f5-d543d5be83f9
+
+
+
+const seller = "?_seller=true"
+// https://v2.api.noroff.dev/auction/listings?_seller=true
+
+
