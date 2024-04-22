6fe4c20e0e4855652b3cc9532d0524bf094505cb
AniTei <115176414+AniTei@users.noreply.github.com>
2024-04-22 15:01:35 +0200

    notes to myself when starting on profile

--- js/profile/profile.mjs
+++ js/profile/profile.mjs
@@ -3,3 +3,24 @@
 // make request, display user info
 
 // possible to make listing. 
+
+/* User stories to happen on profile:
+- able to log out
+- able to create listing
+- able to edit avatar
+- able to view total credit
+
+A registered user may logout
+A registered user may update their avatar
+A registered user may view their total credit
+
+ */
+
+// make relevant info show up:
+// user name
+
+
+// documentation
+/* https://docs.noroff.dev/docs/v2/auction-house/profiles
+ */
+console.log("hello profile :))");
