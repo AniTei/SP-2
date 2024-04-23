09f70c2b22b5f828a0688e4f1e3ed6a1c5736e53
AniTei <115176414+AniTei@users.noreply.github.com>
2024-04-23 23:01:06 +0200

    tried to create api-key, got response 201 ok :)

--- js/profile/profile.mjs
+++ js/profile/profile.mjs
@@ -24,3 +24,67 @@ A registered user may view their total credit
 /* https://docs.noroff.dev/docs/v2/auction-house/profiles
  */
 console.log("hello profile :))");
+
+//pass profile by querystring?? 
+// or locale storage?
+
+
+import { baseURL } from "../endpoints.mjs";
+const exampleProfile =  "/auction/profiles/<bamse_mums>"
+
+const token = localStorage.getItem('token');
+console.log("token:", token)
+
+
+
+////////////////////////////////API KEY//////////////////////////////////////
+
+//error: Object { message: "No API key header was found" }
+
+/* API Key
+Create and use an API Key to access the Noroff API. 
+
+https://docs.noroff.dev/docs/v2/auth/api-key
+
+*/
+
+const apiKey = "/auth/create-api-key"
+const url = baseURL + apiKey;
+console.log("url:",url)
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+    },  
+    body: JSON.stringify({
+        "name": "My API Key name" // Optional
+      }),
+    /* body: JSON.stringify(), */
+
+    //documentation says: A successful response will return a 201 Created status code and the newly created API Key.
+    ////// I GOT A 201 response OK???!!!! :)
+}
+
+async function createApiKey(url, options) {
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    console.log("response:", response)
+    console.log ("data:", data)
+
+};
+createApiKey(url, options);
+
+//error in response, internal server error, 500
+
+/* {
+    "data": {
+      "name": "My API Key name", // Or "API Key" if no name was provided
+      "status": "ACTIVE",
+      "key": "be4ab55c-d5b0-44c3-8a11-67a7dafddd10" // The API Key
+    },
+    "meta": {}
+  } */
