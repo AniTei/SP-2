544e85850bb57f8607074eaeb6f75c3ac06ebaea
AniTei <115176414+AniTei@users.noreply.github.com>
2024-04-24 13:59:31 +0200

    api-key can be created, response ok,

---
+++ js/profile/apiKey.mjs
@@ -0,0 +1,37 @@
+//now i get response ok igjen
+
+console.log("hello create-listing, working on api-key :))");
+
+import { baseURL } from "../endpoints.mjs";
+import { apiKey } from "../endpoints.mjs";
+const url = baseURL + apiKey;
+console.log("url:",url)
+
+const token = localStorage.getItem('token');
+console.log("token:", token)
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+    },  
+    body: JSON.stringify({
+        "name": "My API Key name" 
+      }),
+}
+
+async function createApiKey(url, options) {
+
+    const response = await fetch(url, options);
+    const apiKey = await response.json();
+
+    console.log("response:", response)
+    console.log ("key:", apiKey.data.key)
+
+    return apiKey
+};
+createApiKey(url, options);
+
+
+// i get the right response, now how do i get thet key out of the function?
